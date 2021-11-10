bd91cceb50172f425bcd224d3b71b726206a9480
Rodrigo <rodrigoormonde@gmail.com>
2021-11-10 00:21:07 -0300

    Insert Client

--- backend/src/main/java/com/bootcamp/desafio/controllers/ClientController.java
+++ backend/src/main/java/com/bootcamp/desafio/controllers/ClientController.java
@@ -1,13 +1,17 @@
 package com.bootcamp.desafio.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bootcamp.desafio.dto.ClientDTO;
 import com.bootcamp.desafio.services.ClientService;
@@ -29,6 +33,13 @@ public class ClientController {
 	public ResponseEntity<ClientDTO> findByID(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri(); 
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
--- backend/src/main/java/com/bootcamp/desafio/services/ClientService.java
+++ backend/src/main/java/com/bootcamp/desafio/services/ClientService.java
@@ -34,4 +34,20 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setName(dto.getName());
+	}
+
 }
